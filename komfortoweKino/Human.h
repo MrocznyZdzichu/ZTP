9351a04ee0b9f7864e3c86232da4966f5e6e1d65
MrocznyZdzichu <276786@pw.edu.pl>
2019-04-04 00:28:13 +0200

    Human static fields added

--- komfortoweKino/Human.h
+++ komfortoweKino/Human.h
@@ -6,6 +6,8 @@ class Human : public Component
 {
 public:
     Human();
+    static constexpr double thermalResistance = 0.3;
+    static constexpr double temperature = 273.15 + 36.6;
     virtual double  changeCO2();
     virtual double  changeHumidity();
     virtual double  changeTemperature();
